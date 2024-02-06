cf14b528e287e7c445006428e9f6cc40d6e13016
Landon Navarre <92194808+LandonNav@users.noreply.github.com>
2024-02-06 10:25:27 -0500

    CourseInfo Headers
    Added header change for all courses in course list. Also added brief description of non-EECS courses.

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
 
-function rdicChange()
-{
-    document.getElementById("CourseInfo").innerHTML = "EECS1001 - Research Directions in Computing"
+
+//Below describes the methods to change text in the courseInfo box.
+function rdicChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS1000 - Research Directions in Computing"
 }
+
+function cancaChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS1012 - Computing: a Net-Centric Approach"
+}
+
+function dmfcsChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS1019 - Discrete Mathmatics for Computer Science"
+}
+
+function pfmcChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS1022 - Programming for Mobile Computing"
+}
+
+function itlfcsChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS1090 - Introduction to Logic for Computer Science"
+}
+
+function itttocChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS2001 - Introduction to the Theory of Computation"
+}
+
+function coChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS2021 - Computer Organization"
+}
+
+function aoopChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS2030 - Adcanced Object-Oriented Programming"
+}
+
+function stChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS2031 - Software Tools"
+}
+
+function fodsChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS2101 - Fundamentals of Data Strucutres"
+}
+
+function ppicChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS3000 - Professional Practice in Computing"
+}
+
+function osfChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS3221 - Operating System Fundamentals"
+}
+
+function sdChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS3111 - Software Design"
+}
+
+function uiChange(){
+    document.getElementById("CourseHeader").innerHTML = "EECS3461 - User Interfaces"
+}
+//End of courseInfo change methods.
