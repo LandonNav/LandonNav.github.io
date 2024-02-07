bff1313f366fe704cf1633da6775c5d0fa04be69
Landon Navarre <92194808+LandonNav@users.noreply.github.com>
2024-02-07 16:28:01 -0500

    Large Changes Listed
    Added course summaries to all courses.
    
    Added banner image, some recolouring to match banner.

--- index.js
+++ index.js
@@ -3,57 +3,83 @@
 //Below describes the methods to change text in the courseInfo box.
 function rdicChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS1000 - Research Directions in Computing"
+    document.getElementById("CourseSummary").innerHTML = "This was a small course designed to introduce students to the domains of computer science, both in research and industry, and what those areas are working towards."
 }
 
 function cancaChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS1012 - Computing: a Net-Centric Approach"
+    document.getElementById("CourseSummary").innerHTML = "This was an introductory course in front-end web development."
 }
 
 function dmfcsChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS1019 - Discrete Mathmatics for Computer Science"
+    document.getElementById("CourseSummary").innerHTML = "This was an introductory course in discrete mathmatics relevant to computer science. The course was divided roughly in thirds, focused on propositional logic, set theory, and graph theory, respectively."
+
 }
 
 function pfmcChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS1022 - Programming for Mobile Computing"
+    document.getElementById("CourseSummary").innerHTML = "This was an introductory course in object-oriented programming, with a focus on mobile app development."
+
 }
 
 function itlfcsChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS1090 - Introduction to Logic for Computer Science"
+    document.getElementById("CourseSummary").innerHTML = "Carrying over from EECS1019, this was a more advanced course in formal logic. Going in more depth with propositional logic, and exapanding into modal and temporal logic."
+
 }
 
 function itttocChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS2001 - Introduction to the Theory of Computation"
+    document.getElementById("CourseSummary").innerHTML = "This was an introductory course in computation/complexity theory, the course was divided roughly in thirds, focused on automata theory, turing machines, and formal grammars, respectively."
+
 }
 
 function coChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS2021 - Computer Organization"
+    document.getElementById("CourseSummary").innerHTML = "This was a course in computer architecture and organization. The course was divided roughly in half, first covering hardware - the CPU in particular, followed by covering development in RISC-V assembly."
+
 }
 
 function aoopChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS2030 - Adcanced Object-Oriented Programming"
+    document.getElementById("CourseSummary").innerHTML = "Carrying on from EECS1022, this was a more advanced course in object-oriented programming, covering everything short of synchronization. My professor for this course also added particular emphasis on writing secure applications."
+
 }
 
 function stChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS2031 - Software Tools"
+    document.getElementById("CourseSummary").innerHTML = "This was a course of split focus: The first half of the course was focused on development with C, particularly using pointers. The second half was focused on working with Linux, particularly in shell scripting."
+
 }
 
 function fodsChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS2101 - Fundamentals of Data Strucutres"
+    document.getElementById("CourseSummary").innerHTML = "This was a course studying abstract data structures. I hesitate to say it was introductory, as we covered 40+ structures in good detail. Graph theory was also a focus later in the course."
+
 }
 
 function ppicChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS3000 - Professional Practice in Computing"
+    document.getElementById("CourseSummary").innerHTML = "This was a course in computer ethics, as mandated by the ACM and IEEE. It covered a wide breadth of topics in computer ethics, privcay, and security. Guest lectures made up about half of the lecture-time therein."
+
 }
 
 function osfChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS3221 - Operating System Fundamentals"
+    document.getElementById("CourseSummary").innerHTML = "This was a course discussing the functions and components of operating systems, as well as their development."
+
 }
 
 function sdChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS3111 - Software Design"
+    document.getElementById("CourseSummary").innerHTML = "This was a course on the software design process at a high-level. Methodologies, design patterns, and auxillary tools were covered."
+
 }
 
 function uiChange(){
     document.getElementById("CourseHeader").innerHTML = "EECS3461 - User Interfaces"
+    document.getElementById("CourseSummary").innerHTML = "This was a course in the design and analysis of user interfaces and user experience. Particular focus was given to conducting research towards studying human-computer-interaction."
+
 }
 //End of courseInfo change methods.
